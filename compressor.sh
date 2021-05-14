ce572181557db22177d7daf0c4d896b39aafcc32
JadeVane <wenjinyu@wenjinyu.me>
2021-05-14 15:32:46 +0800

    Fix theme name error

--- compressor.sh
+++ compressor.sh
@@ -3,8 +3,8 @@
 # Select which theme to copy will be automatically transferred to the clipboard,
 # Only valid in linux, and you should install xclip by hand
 # Value: clear, night
-# clip=clear
-clip=night
+clip=clear
+# clip=night
 
 notab=true
 # notab=false
@@ -34,21 +34,21 @@ for (( i = 0; i <= ${#theme[@]}; i++ )); do
 
     elif [[ ${theme[i]} == "night" ]]; then
 
-        content_raw=`sed -r -e '/Default mode start/,/Default mode end/d' \
-                            -e '/No tabs version start/,/No tabs version end/d' \
-                            ${source_file}`
-        content_raw_notab=`sed -r -e '/Default mode start/,/Default mode end/d' \
-                                  -e '/With tabs version start/,/With tabs version end/d' \
-                                  ${source_file}`
-
         # content_raw=`sed -r -e '/Default mode start/,/Default mode end/d' \
-        #                     -e 's/(theme-allure-)clear/\1night/g' \
         #                     -e '/No tabs version start/,/No tabs version end/d' \
         #                     ${source_file}`
         # content_raw_notab=`sed -r -e '/Default mode start/,/Default mode end/d' \
-        #                           -e 's/(theme-allure-)clear/\1night/g' \
         #                           -e '/With tabs version start/,/With tabs version end/d' \
         #                           ${source_file}`
+
+        content_raw=`sed -r -e '/Default mode start/,/Default mode end/d' \
+                            -e 's/(theme-allure-)clear/\1night/g' \
+                            -e '/No tabs version start/,/No tabs version end/d' \
+                            ${source_file}`
+        content_raw_notab=`sed -r -e '/Default mode start/,/Default mode end/d' \
+                                  -e 's/(theme-allure-)clear/\1night/g' \
+                                  -e '/With tabs version start/,/With tabs version end/d' \
+                                  ${source_file}`
 
     else
         exit 0;
