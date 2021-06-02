2d29dc28734b543fea73cb3b998821e78779e603
JadeVane <wenjinyu@wenjinyu.me>
2021-06-02 17:35:54 +0800

    Move sync button to left sidebar

--- compressor.sh
+++ compressor.sh
@@ -34,21 +34,21 @@ for (( i = 0; i <= ${#theme[@]}; i++ )); do
 
     elif [[ ${theme[i]} == "night" ]]; then
 
-        content_raw=`sed -r -e '/Default mode start/,/Default mode end/d' \
-                            -e '/No tabs version start/,/No tabs version end/d' \
-                            ${source_file}`
-        content_raw_notab=`sed -r -e '/Default mode start/,/Default mode end/d' \
-                                  -e '/With tabs version start/,/With tabs version end/d' \
-                                  ${source_file}`
-
         # content_raw=`sed -r -e '/Default mode start/,/Default mode end/d' \
-        #                     -e 's/(theme-allure-)clear/\1night/g' \
         #                     -e '/No tabs version start/,/No tabs version end/d' \
         #                     ${source_file}`
         # content_raw_notab=`sed -r -e '/Default mode start/,/Default mode end/d' \
-        #                           -e 's/(theme-allure-)clear/\1night/g' \
         #                           -e '/With tabs version start/,/With tabs version end/d' \
         #                           ${source_file}`
+
+        content_raw=`sed -r -e '/Default mode start/,/Default mode end/d' \
+                            -e 's/(theme-allure-)clear/\1night/g' \
+                            -e '/No tabs version start/,/No tabs version end/d' \
+                            ${source_file}`
+        content_raw_notab=`sed -r -e '/Default mode start/,/Default mode end/d' \
+                                  -e 's/(theme-allure-)clear/\1night/g' \
+                                  -e '/With tabs version start/,/With tabs version end/d' \
+                                  ${source_file}`
 
     else
         exit 0;
